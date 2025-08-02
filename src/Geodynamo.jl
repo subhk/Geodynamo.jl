7ea97fc1824df6acb8477cee03e6645baab6c6d4
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-02 16:59:17 +0300

    Update Geodynamo.jl

--- src/Geodynamo.jl
+++ src/Geodynamo.jl
@@ -1,5 +1,18 @@
 module Geodynamo
 
-# Write your package code here.
+    using LinearAlgebra
+    using SparseArrays
+    using MPI
+    using PencilArrays
+    using PencilFFTs
+    using HDF5
+    using StaticArrays
+    using SHTnsSpheres
+
+
+    # export shtns_config.jl
+    export SHTnsConfig, create_shtns_config, create_parallel_shtns_config
+
+    
 
 end
