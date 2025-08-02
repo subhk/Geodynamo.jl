bfccdb8bc84b7057419d41d14c3a4682e152a5df
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-02 17:11:31 +0300

    Update Geodynamo.jl

--- src/Geodynamo.jl
+++ src/Geodynamo.jl
@@ -16,10 +16,16 @@ module Geodynamo
     # exports pencil_decomps.jl
     export create_pencil_topology, create_transforms, comm
 
+
     # exports field.jl
     export SHTnsSpectralField, SHTnsPhysicalField, SHTnsVectorField, SHTnsTorPolField
     export RadialDomain, create_shtns_spectral_field, create_shtns_physical_field
     export create_shtns_vector_field, create_radial_domain 
+
+
+    # exports linear_algebra.jl
+    export BandedMatrix, create_derivative_matrix, create_radial_laplacian, apply_banded_matrix!
+
 
 
     
